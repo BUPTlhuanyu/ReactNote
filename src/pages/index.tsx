b1a8f2dce299ab92516f1b2645a02095e09fcfa5
liaohuanyu <liaohuanyu@baidu.com>
2021-10-27 11:04:05 +0800

    feat: 统计

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,14 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?64a3c7fa9e8533c72827dc22e0d273ab";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
